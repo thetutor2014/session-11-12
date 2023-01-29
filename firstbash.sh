e9227b93c33975e4708c0c28e7288dd47feffa3f
Alireza Rezaee <120360813+thetutor2014@users.noreply.github.com>
2023-01-29 10:52:52 +0330

    Add files via upload

---
+++ firstbash.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+touch newfile.sh /home/alireza1
+touch
+greeting=Hello
+name=User
+echo $greeting $name
+echo "Enter a numner"
+read a
+echo "Enter a numner"
+read b
+var=$((a+b))
+echo $var
+echo " YOU HAVE BEEN HACKED "
